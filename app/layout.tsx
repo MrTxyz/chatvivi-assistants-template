d9b4dabb9810747713bdee30009f6bd6411607ca
MrTxyz <35548354+MrTxyz@users.noreply.github.com>
2025-08-26 19:00:01 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_APP_NAME || 'ChatVivi AI Agent',
-  description: 'AI-Agent design by T&T Solutions'
+  description: 'AI Agent design by T&T Solutions'
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
